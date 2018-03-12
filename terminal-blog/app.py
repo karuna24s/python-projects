856b805195c3969081be972c9d244ea6ee7ccbb8
karuna24s <karuna24s@gmail.com>
2018-03-11 21:10:04 -0400

    add title, content, author parameters to the initialize method, so that way we can create a new object (post) already initialized with some properties that we know beforehand

--- terminal-blog/app.py
+++ terminal-blog/app.py
@@ -1,4 +1,8 @@
 from models.post import Post
 
-post = Post()
-post2 = Post()
+post = Post("Post1 title", "Post1 content", "Post1 author")
+post2 = Post("Post2 title", "Post2 content", "Post2 author")
+
+print(post.content)
+
+print(post2.content)
--- terminal-blog/models/post.py
+++ terminal-blog/models/post.py
@@ -2,7 +2,7 @@ class Post(object):
     # init methods stands for initialize and is the method that gets called when
     # when you say you want to create a new thing called a post and this thing
     # is going to have the following content and title
-    def __init__(self):
-        self.title = "Title"
-        self.content = "Content"
-        self.author = "Karuna"
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
